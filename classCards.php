f481157568630d86bfb303b1faa4f63cd58e8ae3
UxerDevCoding <omarheviaarbana@gmail.com>
2022-01-10 20:56:50 +0100

    foreach method doesn't work thus multiple cards were created

--- classCards.php
+++ classCards.php
@@ -8,7 +8,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-
     <?php
 
     class Movie
@@ -52,27 +51,78 @@
         $movie3 = new Movie ("Jaws", "Steven Spielberg", "1975","assets\movieJaws.jpg" );
         $movie4 = new Movie ("Pulp Fiction", "Quentin Tarantino", "1994","assets\moviePulp.jpg");
         $movie5 = new Movie ("Star Wars", "George Lucas", "1977", "assets\movieStar.jpg");
-        $movies = [$movie1, $movie2, $movie3, $movie4, $movie5];
+        // $movies = [$movie1, $movie2, $movie3, $movie4, $movie5];   
         
-        
-        ?>
-        
-        <?php
-            
-        foreach ($movies as $dataMovie) 
-
+        // foreach ($movies as $dataMovie);
         ?>
 
-        <div class="card" style="width: 18rem;">
-            <img src="<?php echo $dataMovie->getPoster();?> " class="card-img-top" alt="Poster">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo $dataMovie-> getTitle();?></h5>
+        <div class="container overflow-hidden">    
+            <div class="row row-cols-1 row-cols-md-3 g-4 ">
+                <div class="col">
+                        <div class="card p-3 border bg-light" style="width: 18rem;">
+                            <img src="<?php echo $movie1->getPoster();?> " class="card-img-top" alt="Poster">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $movie1-> getTitle();?></h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Director: <?php echo $movie1->getDirector();?></li>
+                                <li class="list-group-item">Year: <?php echo $movie1-> getYear();?></li>
+                            </ul>  
+                        </div>
+                </div>
+
+                <div class="col">
+                        <div class="card p-3 border bg-light" style="width: 18rem;">
+                            <img src="<?php echo $movie2->getPoster();?> " class="card-img-top" alt="Poster">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $movie2-> getTitle();?></h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Director: <?php echo $movie2->getDirector();?></li>
+                                <li class="list-group-item">Year: <?php echo $movie2-> getYear();?></li>
+                            </ul>  
+                        </div>
+                </div>
+
+                <div class="col">
+                        <div class="card p-3 border bg-light" style="width: 18rem;">
+                            <img src="<?php echo $movie3->getPoster();?> " class="card-img-top" alt="Poster">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $movie3-> getTitle();?></h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Director: <?php echo $movie3->getDirector();?></li>
+                                <li class="list-group-item">Year: <?php echo $movie3-> getYear();?></li>
+                            </ul>  
+                        </div>
+                </div>
+
+                <div class="col">
+                        <div class="card p-3 border bg-light" style="width: 18rem;">
+                            <img src="<?php echo $movie4->getPoster();?> " class="card-img-top" alt="Poster">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $movie4-> getTitle();?></h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Director: <?php echo $movie4->getDirector();?></li>
+                                <li class="list-group-item">Year: <?php echo $movie4-> getYear();?></li>
+                            </ul>  
+                        </div>
+                </div>
+
+                <div class="col">
+                        <div class="card p-3 border bg-light" style="width: 18rem;">
+                            <img src="<?php echo $movie5->getPoster();?> " class="card-img-top" alt="Poster">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $movie5-> getTitle();?></h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Director: <?php echo $movie5->getDirector();?></li>
+                                <li class="list-group-item">Year: <?php echo $movie5-> getYear();?></li>
+                            </ul>  
+                        </div>
+                </div>
             </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Director: <?php echo $dataMovie->getDirector();?></li>
-                <li class="list-group-item">Year: <?php echo $dataMovie-> getYear();?></li>
-            </ul> 
-        </div>
-
+        </div> 
     </body>
 </html>
